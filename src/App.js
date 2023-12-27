2ddf7b81e55af12261e9413649cd84a421ad3cd0
vikash rajpoot <vikashraj1490@gmail.com>
2023-12-28 03:05:50 +0530

    addmovie

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ function App() {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
   const [retry, setRetry] = useState(true)
+  const [formdata, setFormData] = useState({})
 
-  
+  console.log("dummyMovies",dummyMovies)
+
   async function fetchresult() {
     try {
       setLoading(true)
@@ -30,23 +32,56 @@ function App() {
       fetchresult()
     }, 5000)
   }
-  
+
   useEffect(() => {
     if (retry) {
       fetchresult()
     }
   }, [retry])
-  
+
   const cancelRetry = () => {
     setRetry(false)
   }
+
+  const handleChange = (e) => {
+    setFormData((prev)=>({
+      ...prev,[e.target.id]:e.target.value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formdata);
+    console.log(dummyMovies)
+    setDummyMovies((prev)=> [...prev,formdata])
+  }
+  
   return (
     <React.Fragment>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div className='input'>
+            <label>Title</label>
+            <input id='title' onChange={handleChange} type='text' />
+          </div>
+          <div className='input'>
+            <label>opening Text</label>
+            <textarea id='openingText' onChange={handleChange} rows={6} cols={50} />
+          </div>
+          <div className='input'>
+            <label>release Date</label>
+            <input onChange={handleChange} id='releaseDate' type='text' />
+          </div>
+          <div>
+            <button type='submit'>Add Movie</button>
+          </div>
+        </form>
+      </section>
       <section>
         <button>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList loading={loading} movies={dummyMovies} />
+        {/* <MoviesList loading={loading} movies={dummyMovies} /> */}
         {!loading && error && "Something went wrong ! ...Retrying"}
         {retry && loading && <button onClick={cancelRetry} >Cancel Retry</button>}
       </section>
